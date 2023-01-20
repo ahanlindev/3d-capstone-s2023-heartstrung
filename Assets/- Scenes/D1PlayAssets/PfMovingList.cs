f4cab2f9198e35d56fec783e415a69efce39f8f7
DD-1111 <beastblackga@outlook.com>
2023-01-20 17:56:15 -0600

    two ways of platform moving implemented, also attached object to platform while moving

---
+++ Assets/- Scenes/D1PlayAssets/PfMovingList.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PfMovingList : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 2; //
+    public Transform[] target;  //
+    public float delta = 0.2f; // 
+    private static int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveTo();
+    }
+
+
+    void moveTo()
+    {
+
+        target[i].position = new Vector3(target[i].position.x, transform.position.y, target[i].position.z);
+
+
+        transform.LookAt(target[i]);
+
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        if (transform.position.x > target[i].position.x - delta
+            && transform.position.x < target[i].position.x + delta
+            && transform.position.z > target[i].position.z - delta
+            && transform.position.z < target[i].position.z + delta)
+            i = (i + 1) % target.Length;
+    }
+}
---
+++ Assets/- Scenes/D1PlayAssets/PlatformMoving.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMoving : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+
+
+    //public float hangningtime = 0f;
+    private Vector3 startingPos;
+ 
+
+  
+    [SerializeField] [Range(0, 5)] float mvFactor;
+    [SerializeField] float period = 2f;
+    Transform collidedObj;
+    private static float magicMagnitude = 1.6f;
+    public Transform wayPointOne;
+    public Transform wayPointTwo;
+
+
+    void Start()
+    {
+       
+        startingPos = wayPointOne.position;
+       
+    }
+
+    void FixedUpdate()
+    {
+        float cycles = Time.time / period;
+
+        const float tau = Mathf.PI * 2;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        mvFactor = (rawSinWave) * magicMagnitude;
+        Vector3 mvVector = wayPointTwo.position - wayPointOne.position;
+        Vector3 offset = mvVector * mvFactor * Time.fixedDeltaTime;
+        transform.position += offset;
+        Debug.Log(offset);
+
+        if (collidedObj != null)
+        {
+            collidedObj.GetComponent<Rigidbody>().transform.position += offset;
+        }
+       
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collidedObj = collision.collider.transform;
+
+    }
+}
