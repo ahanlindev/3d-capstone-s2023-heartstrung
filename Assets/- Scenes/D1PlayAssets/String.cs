a884208ff545b7917253498539ac4cdb60b79ead
DD-1111 <beastblackga@outlook.com>
2023-01-21 18:21:52 -0600

    fixed platforms and integrated a general platform
    11

--- Assets/- Scenes/D1PlayAssets/String.cs
+++ Assets/- Scenes/D1PlayAssets/String.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StringManager : MonoBehaviour
+public class String : MonoBehaviour
 {
 	// Start is called before the first frame update
 
