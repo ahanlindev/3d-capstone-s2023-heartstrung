bfb249275fec0b01db9820e0ad9e8a99fd673504
Alex Hanlin <alexhanlin@utexas.edu>
2023-01-21 18:15:16 -0600

    Untested fling mechanic

--- Assets/- Scripts/Player/Heart.cs
+++ Assets/- Scripts/Player/Heart.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using DG.Tweening;
 
 [RequireComponent(typeof(Rigidbody), typeof(Collider))]
 public class Heart : MonoBehaviour
@@ -11,6 +12,9 @@ public class Heart : MonoBehaviour
     [Tooltip("PlayerController instance for the player attached to this object")]
     [SerializeField] private PlayerController player;
     
+    [Tooltip("Linear speed along the arc that the heart will fly through the air in units per second")]
+    [SerializeField] private float flingSpeed = Mathf.PI;
+
     // private fields
     // rigidbody of the heart
     private Rigidbody rbody;
@@ -18,9 +22,17 @@ public class Heart : MonoBehaviour
     // collider of the heart
     private Collider coll;
     
-    // true when the heart is actively being flung. 
-    [SerializeField] private State currentState;
+    // current state of the heart. 
+    [SerializeField] private State currentState; // TODO deserialize after debug
 
+    // Used when flinging. Fling destination as a point in space.
+    private Vector3 currentDestination;
+    
+    // Used when flinging. Angle in degrees from heart's start point to destination, using player as the pivot.
+    private float totalFlingAngle;
+
+    // Used when flinging. Radius in units between player and heart when the fling begins
+    private float initialFlingRadius;
 
     // Emitted events
     public static event Action LandedEvent;
@@ -45,28 +57,82 @@ public class Heart : MonoBehaviour
 
     public bool CanBeFlung() => CanChangeState(State.FLUNG);
 
+    // Per-tick updates
+    
+    private void FixedUpdate() {
+        if (currentState == State.FLUNG) {
+            
+        }    
+    }
+
     // Event Handlers
 
     /// <summary>Subscribes to the player's fling event. Will fling this object around the player</summary>
     void OnPlayerFling(float power) {
         Debug.Log($"Flung by player with power {power}");
         //DEBUG
-        StartCoroutine(DEBUGFlingTimer());
+
+        if (TryChangeState(State.FLUNG)) {
+            currentDestination = CalculateDestination(power);
+            totalFlingAngle = CalculateAngleToDestination(currentDestination);
+            initialFlingRadius = Vector3.Distance(transform.position, player.transform.position);
+        }
+    }
+    
+    // DEBUG
+    /// <summary>
+    /// Moves the object around the player towards the destination over the course of a single physics tick
+    ///</summary>
+    private void DoFlingTick() { 
+        if (transform.position != currentDestination) {
+
+            // move orthogonal to player and their left vector. Will result in an arc over their head
+            Vector3 vecFromPlayer = transform.position - player.transform.position;
+            Vector3 moveDir = Vector3.Cross(vecFromPlayer, -player.transform.right).normalized;
+            Vector3 movement = moveDir * flingSpeed * Time.fixedDeltaTime;
+
+            // apply linear movement
+            Vector3 newPos = transform.position + movement;
+
+            // pull new position to within proper radius
+            float remainingAngle = CalculateAngleToDestination(currentDestination);
+            float radiusLerpRatio = remainingAngle / totalFlingAngle;
+            
+            Vector3 newVecFromPlayer = newPos - player.transform.position;
+            float destRadius = Vector3.Distance(player.transform.position, currentDestination);
+            float newRadius = Mathf.Lerp(initialFlingRadius, destRadius, radiusLerpRatio);
+            newPos = player.transform.position + (newVecFromPlayer.normalized) * newRadius;
+            rbody.MovePosition(newPos);
+        }
+        else {
+            TryFinishFling();
+        }
+    }
+
+    /// <summary>
+    /// Attempts to go from FLUNG to another state. 
+    /// Usually idle or falling. Emits LandedEvent if successful.
+    /// </summary>
+    private bool TryFinishFling() {
+        if (IsGrounded()) {
+            // Heart has landed. Go into idle mode and emit event
+            TryChangeState(State.IDLE);
+            LandedEvent?.Invoke();
+            return true;
+        } else {
+            // Heart has hit a wall. Can't finish yet. Start falling.
+            TryChangeState(State.FALLING);
+            return false;
+        }
     }
 
     private void OnCollisionEnter(Collision other) {
         // hit wall or floor
-        if (currentState == State.FLUNG) {
-            if (IsGrounded()) {
-                TryChangeState(State.IDLE);
-            } else {
-                TryChangeState(State.FALLING);
-            }
+        if (currentState == State.FLUNG || currentState == State.FALLING) {
+            TryFinishFling();
         }
     }
 
-    // DEBUG
-    private IEnumerator DEBUGFlingTimer() { yield return new WaitForSeconds(2.0f); LandedEvent?.Invoke(); }
 
     // State management
 
@@ -139,12 +205,31 @@ public class Heart : MonoBehaviour
         Vector3 playerPos = player.transform.position;
         Vector3 pos = transform.position;
 
-        // find vector to player
+        // find vector from heart to player
         var vecToPlayer = playerPos - pos;
         
         // destination is radius between player and heart, in the forward dir of the player, scaled by power.
         var vecFromPlayer = player.transform.forward * vecToPlayer.magnitude * power;
         return Vector3.zero;
+    }
+
+    /// <summary>
+    /// Returns the angle in degrees between the heart and 
+    /// its destination, with the player's position as the origin
+    /// </summary>
+    /// <param name="destination">The desired destination position</param>
+    float CalculateAngleToDestination(Vector3 destination) {
+        Vector3 playerPos = player.transform.position;
+        Vector3 pos = transform.position;
+
+        // find vectors from player to heart, and from player to destination
+        var playerVecToHeart = pos - playerPos;
+        var playerVecToDest = destination - playerPos;
+
+        // get angles to these vectors with respect to Player's up vector
+        // and add them together to get total angle to destination
+        return Vector3.Angle(playerVecToHeart, player.transform.up) 
+                + Vector3.Angle(player.transform.up, playerVecToDest);
     }
 
     /// <summary>Checks if heart is grounded using a raycast.</summary>
