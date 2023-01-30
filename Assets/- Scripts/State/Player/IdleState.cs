b4f033813228b438da20373382be820308cbf88d
Alex Hanlin <gluethulhu@gmail.com>
2023-01-30 10:46:26 -0600

    Basic idle and moving states

--- Assets/- Scripts/State/Player/IdleState.cs
+++ Assets/- Scripts/State/Player/IdleState.cs
@@ -7,24 +7,22 @@ namespace Player
     {
         public IdleState(PlayerStateMachine stateMachine) : base("Idle", stateMachine) { }
 
+        public override void UpdateLogic() => base.UpdateLogic();
+        public override void UpdatePhysics() => base.UpdatePhysics();
+        protected override void OnHurt() => base.OnHurt();
+        protected override void OnDie() => base.OnDie();
+        protected override void OnHeartLanded() => base.OnHeartLanded();
+
         public override void Enter()
         {
             base.Enter();
-        }
-
-        public override void UpdateLogic()
-        {
-            base.UpdateLogic();
-        }
-
-        public override void UpdatePhysics()
-        {
-            base.UpdatePhysics();
+            _stateMachine.anim.SetBool("Idle", true);
         }
 
         public override void Exit()
         {
             base.Exit();
+            _stateMachine.anim.SetBool("Idle", false);
         }
 
         protected override void OnPlayerAttack(CallbackContext _)
@@ -36,21 +34,26 @@ namespace Player
         protected override void OnPlayerJump(CallbackContext _)
         {
             base.OnPlayerJump(_);
+            // TODO this might need rework if we implement a "Jumping" state
+            _stateMachine.ChangeState(_stateMachine.movingState);
         }
 
         protected override void OnPlayerStartFling(CallbackContext _)
         {
             base.OnPlayerStartFling(_);
+            _stateMachine.ChangeState(_stateMachine.chargingState);
         }
 
-        protected override void OnPlayerFinishFling(CallbackContext _)
-        {
-            base.OnPlayerFinishFling(_);
-        }
+        protected override void OnPlayerFinishFling(CallbackContext _) => base.OnPlayerFinishFling(_);
 
         protected override void HandlePlayerMove(Vector3 moveVector)
         {
             base.HandlePlayerMove(moveVector);
+            if (moveVector != Vector3.zero)
+            {
+                _stateMachine.ChangeState(_stateMachine.movingState);
+            }
         }
+
     }
 }
--- Assets/- Scripts/State/Player/MovingState.cs
+++ Assets/- Scripts/State/Player/MovingState.cs
@@ -7,49 +7,51 @@ namespace Player
     {
         public MovingState(PlayerStateMachine stateMachine) : base("Moving", stateMachine) { }
 
+        public override void UpdateLogic() => base.UpdateLogic();
+        public override void UpdatePhysics() => base.UpdatePhysics();
+        protected override void OnPlayerStartFling(CallbackContext _) => base.OnPlayerStartFling(_);
+        protected override void OnPlayerFinishFling(CallbackContext _) => base.OnPlayerFinishFling(_);
+        protected override void OnHurt() => base.OnHurt();
+        protected override void OnDie() => base.OnDie();
+        protected override void OnHeartLanded() => base.OnHeartLanded();
+        
         public override void Enter()
         {
             base.Enter();
-        }
-
-        public override void UpdateLogic()
-        {
-            base.UpdateLogic();
-        }
-
-        public override void UpdatePhysics()
-        {
-            base.UpdatePhysics();
+            _stateMachine.anim.SetBool("Moving", true);
         }
 
         public override void Exit()
         {
             base.Exit();
+            _stateMachine.anim.SetBool("Moving", false);
         }
 
         protected override void OnPlayerAttack(CallbackContext _)
         {
             base.OnPlayerAttack(_);
+            _stateMachine.ChangeState(_stateMachine.attackingState);
         }
 
         protected override void OnPlayerJump(CallbackContext _)
         {
+            // TODO implement jump
             base.OnPlayerJump(_);
-        }
-
-        protected override void OnPlayerStartFling(CallbackContext _)
-        {
-            base.OnPlayerStartFling(_);
-        }
-
-        protected override void OnPlayerFinishFling(CallbackContext _)
-        {
-            base.OnPlayerFinishFling(_);
         }
 
         protected override void HandlePlayerMove(Vector3 moveVector)
         {
             base.HandlePlayerMove(moveVector);
+
+            // account for player move speed and tick rate
+            moveVector *= _stateMachine.moveSpeed;
+            moveVector *= Time.fixedDeltaTime;
+
+            // find proper position and look rotation
+            var newPos = _stateMachine.transform.position + moveVector;
+            var newRot = Quaternion.LookRotation(moveVector.normalized, _stateMachine.transform.up);
+
+            _stateMachine.rbody.Move(newPos, newRot);
         }
     }
 }
