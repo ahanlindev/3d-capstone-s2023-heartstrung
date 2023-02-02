e8d0df465edd573774a09c6490ceeba2a389c233
Alex Hanlin <gluethulhu@gmail.com>
2023-02-02 14:20:31 -0600

    Add Health component

---
+++ Assets/- Scripts/Health.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+/// <summary>Measures and updates health. Notifies listeners when health changes.</summary>
+public class Health : MonoBehaviour
+{
+    // Emitted events
+    /// <summary> 
+    /// Emitted when current health changes. 
+    /// First parameter is the new amount. Second parameter is the delta 
+    /// </summary>
+    public event Action<float, float> ChangeHealthEvent;
+    
+    [Tooltip("Max health for this entity")]
+    [SerializeField] private float _maxHealth = 100f;
+
+    /// <summary>current health value</summary>
+    public float CurrentHealth { get; private set; }
+
+
+    void Awake()
+    {
+        CurrentHealth = _maxHealth;        
+    }
+
+    /// <summary>
+    /// Updates the current amount of health by the specified 
+    /// value, and sends a corresponding event. 
+    /// </summary>
+    public void ChangeHealth(float delta) {
+        CurrentHealth += delta;
+        ChangeHealthEvent?.Invoke(CurrentHealth, delta);
+    }
+}
