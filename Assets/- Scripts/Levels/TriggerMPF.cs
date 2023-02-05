7ce1086c299d511ced190ad0ff88df4ba13bda6c
DD-1111 <beastblackga@outlook.com>
2023-02-05 15:28:19 -0600

    Codedup the level logistics and button triggering assigned

---
+++ Assets/- Scripts/Levels/TriggerMPF.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerMPF : MonoBehaviour
+{
+
+    [SerializeField] public Trigger trigger;
+    PfMovingList MPFScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MPFScript = transform.GetChild(0).GetComponent<PfMovingList>();
+        MPFScript.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!MPFScript.enabled)
+        {
+            if (trigger.getStatus())
+            {
+                MPFScript.enabled = true;
+            }
+            else
+            {
+                MPFScript.enabled = false;
+            }
+        }
+      
+    }
+}
