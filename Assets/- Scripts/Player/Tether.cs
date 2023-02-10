9e032202178acb6e31a3b1b4812ff7a6ad1598f3
Alex Hanlin <gluethulhu@gmail.com>
2023-02-09 21:11:05 -0600

    Rename String to Tether

--- Assets/- Scripts/Player/Tether.cs
+++ Assets/- Scripts/Player/Tether.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class String : MonoBehaviour
+public class Tether : MonoBehaviour
 {
 	// Start is called before the first frame update
 
