12cd29f7e1ccbb8266bd4b3ce772c21c52d6c93d
Alex Hanlin <gluethulhu@gmail.com>
2023-02-19 14:57:23 -0600

    Add input-handling and comments to ComicManager

--- Assets/- Scripts/UI/Comics/ComicManager.cs
+++ Assets/- Scripts/UI/Comics/ComicManager.cs
@@ -1,43 +1,97 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.InputSystem.InputAction;
 using UnityEngine.SceneManagement;
 using DG.Tweening;
 
 /// <summary>Controls displaying and hiding comic pages in sequence</summary>
 public class ComicManager : MonoBehaviour
 {
+    /// TODO: replace with version in scene management class
+    [Tooltip("Image used to cover the screen when fading out of the scene. ")]
+    [SerializeField] private RawImage _fadeOutOverlay;
+
     [Tooltip("Time in seconds that each new panel will fade in")]
-    [SerializeField] private float _fadeInTime = 0.5f;
+    [SerializeField] private float _panelFadeInTime = 0.5f;
 
     [Tooltip("Time in seconds that each new panel will fade out")]
-    [SerializeField] private float _fadeOutTime = 0.25f;
+    [SerializeField] private float _panelFadeOutTime = 0.25f;
 
+    [Tooltip("Time in seconds that the player must hold the skip button")]
+    [SerializeField] private float _skipHoldTime = 1.0f;
+    
+
+    /// <summary>Index of current panel</summary>
     private int _index = 0;
 
+    /// <summary>Standard parameters for tweens to fade panels in and out </summary>
     private TweenParams _fadeParams;
+
+    /// <summary>Input bindings for controller/keyboard comic control</summary>
+    private PlayerInput _input;
+
+    /// <summary>Tween used to measure how long the player has been holding skip</summary>
+    private Tween _skipTimer;
+
+    /// <summary>Id string that allows fade tweens to be tracked and controlled</summary>
+    private readonly string _fadeId = "ComicManagerFade";
 
     void Start()
     {
+        // initialize input and input events
+        _input = new PlayerInput();
+        _input.Enable();
+
+        _input.Comic.Next.performed += OnNextInput;
+        _input.Comic.Prev.performed += OnPrevInput;
+        _input.Comic.Skip.performed += OnSkipStartInput;
+        _input.Comic.Skip.canceled += OnSkipCancelInput;
+
+        // ensure fade out image exists, then make it transparent
+        if (!_fadeOutOverlay) { Debug.LogError("fadeOutOverlay is not set in ComicManager!"); }
+        _fadeOutOverlay?.DOFade(endValue: 0f, duration: 0f);
+
         // set all child canvases to be inactive
         foreach (Transform child in transform)
         {
             child.gameObject.SetActive(false);
-            child.GetComponent<RawImage>().material.color = Color.white;
         }
 
-        // initialize fade parameters
+        // initialize panel fade parameters
         _fadeParams = new TweenParams()
-            .SetEase(Ease.InOutCubic);
+            .SetEase(Ease.InOutCubic)
+            .SetId(_fadeId);
 
         // activate the starting index
-        FadeCurrentSlide(fadeIn: true);
+        FadeCurrentPanel(fadeIn: true);
     }
 
     private void OnDestroy()
     {
-        DOTween.KillAll();
+        // cancel all in-progress tweens if relevant
+        DOTween.Kill(_fadeId);
+
+        // teardown input and input events
+        _input.Comic.Next.performed -= OnNextInput;
+        _input.Comic.Prev.performed -= OnPrevInput;
+
+        _input.Disable();
+    }
+    
+    // Input handlers
+    private void OnNextInput(CallbackContext _) => Next();
+    private void OnPrevInput(CallbackContext _) => Previous();
+
+    private void OnSkipStartInput(CallbackContext _) {
+        _skipTimer?.Kill(); // clean up existing tweens
+        _skipTimer = _fadeOutOverlay.DOFade(endValue: 1, duration: _skipHoldTime)
+            .SetAs(_fadeParams)
+            .OnComplete(LoadNextScene);
+    }
+
+    private void OnSkipCancelInput(CallbackContext _) {
+        _skipTimer?.Flip(); // fade back in 
+        _skipTimer?.OnComplete(() => {}); // cancels the scene load
     }
 
     /// <summary>
@@ -48,42 +102,45 @@ public class ComicManager : MonoBehaviour
     {
         if (_index >= transform.childCount - 1)
         {
-            FadeIntoNextScene();
+            // TODO really want a scene-control singleton for this stuff
+            _fadeOutOverlay.DOFade(endValue: 1, duration: _panelFadeInTime)
+                .SetAs(_fadeParams)
+                .OnComplete(LoadNextScene);
         }
         else
         {
             _index++;
-            FadeCurrentSlide(fadeIn: true);
+            FadeCurrentPanel(fadeIn: true);
         }
     }
+
 
     /// <summary>Return to the previous page of the comic, if it exists/</summary>
     public void Previous()
     {
         if (_index > 0)
         {
-            FadeCurrentSlide(fadeIn: false);
+            FadeCurrentPanel(fadeIn: false);
             _index--;
-            //FadeCurrentSlide(fadeIn: true);
         }
     }
 
     /// <summary>Skips the comic and loads the next scene</summary>
     public void Skip()
     {
-        FadeIntoNextScene();
+        LoadNextScene();
     }
 
-    /// <summary>Gradually fades the slide at the current index either in or out</summary>
-    /// <param name="fadeIn">If true, the slide at the current index is enabled, otherwise it is disabled.</param>
-    private void FadeCurrentSlide(bool fadeIn)
+    /// <summary>Gradually fades the comic panel at the current index either in or out</summary>
+    /// <param name="fadeIn">If true, the comic panel at the current index is enabled, otherwise it is disabled.</param>
+    private void FadeCurrentPanel(bool fadeIn)
     {
         // set up variables
         Debug.Log($"index {_index}");
         RawImage image = transform.GetChild(_index)?.GetComponent<RawImage>();
         float startAlpha = (fadeIn) ? 0.0f : 1.0f;
         float endAlpha = (fadeIn) ? 1.0f : 0.0f;
-        float fadeTime = (fadeIn) ? _fadeInTime : _fadeOutTime;
+        float fadeTime = (fadeIn) ? _panelFadeInTime : _panelFadeOutTime;
 
         // fade in or fade out image as appropriate
         Color startColor = image.color;
@@ -108,25 +165,11 @@ public class ComicManager : MonoBehaviour
         }
     }
 
-    /// <summary>Fades out all active slides and loads the next scene</summary>
-    private void FadeIntoNextScene()
+    /// <summary>Fades out all active panels and loads the next scene</summary>
+    // TODO replace with SceneManager
+    private void LoadNextScene()
     {
-        foreach (Transform t in transform)
-        {
-            var image = t.gameObject.GetComponent<RawImage>();
-            if (image && image.isActiveAndEnabled)
-            {
-                image.DOFade(0, _fadeOutTime);
-            }
-        }
-        DOVirtual.DelayedCall(
-            _fadeOutTime * 1.5f,
-            () =>
-            {
-                int index = SceneManager.GetActiveScene().buildIndex;
-                SceneManager.LoadSceneAsync(index + 1);
-            },
-            false
-         );
+        int index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadSceneAsync(index + 1);
     }
 }
