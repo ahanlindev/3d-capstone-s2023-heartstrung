21b8633ff7ae466fb70bbe7be626c80723cc6d66
Alex Hanlin <gluethulhu@gmail.com>
2023-02-21 17:37:46 -0600

    remove debug log

--- Assets/- Scripts/UI/OptionsMenuManager.cs
+++ Assets/- Scripts/UI/OptionsMenuManager.cs
@@ -31,7 +31,6 @@ public class OptionsMenuManager : MonoBehaviour
     }
 
     private void Update() {
-        Debug.Log(EventSystem.current.currentSelectedGameObject );
         if (optionsOpen && EventSystem.current.currentSelectedGameObject == null) {
             defaultSelection?.Select();
         }
