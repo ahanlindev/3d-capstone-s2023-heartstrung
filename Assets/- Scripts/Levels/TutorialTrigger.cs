844001f0c52c4d6bf24c7ff76704e5847be5677f
DD-1111 <56623337+DD-1111@users.noreply.github.com>
2023-02-27 22:31:19 -0600

    Finish polishing 2.3 2.4, changed camera angles, Fix a bug that 2.4's bubble doesn't show later hint sometime.

--- Assets/- Scripts/Levels/TutorialTrigger.cs
+++ Assets/- Scripts/Levels/TutorialTrigger.cs
@@ -6,9 +6,15 @@ using DG.Tweening;
 public class TutorialTrigger : MonoBehaviour
 {
 
-    public int HintIndex;
+    public int hintIndex;
+
+    [Tooltip("The time kitty need to pass before triggering the bubble")]
+    public int countDown;
+
     [Tooltip("Targeted UI Bubbles, if empty, will search for name [ChatBubble]")]
     [SerializeField] public TutorialBubble[] tutorialBubbles;
+
+
 
 
     // Start is called before the first frame update
@@ -31,13 +37,15 @@ public class TutorialTrigger : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
-        { 
-            foreach (TutorialBubble tutorialBubble in tutorialBubbles)
+        {
+            if (countDown == 0)
             {
-                Debug.Log(tutorialBubble.gameObject.name);
-                tutorialBubble.changeText(HintIndex);
-            }
-            
+                foreach (TutorialBubble tutorialBubble in tutorialBubbles)
+                {
+                    //Debug.Log("11");
+                    tutorialBubble.changeText(hintIndex);
+                }
+            }     
         }
 
     }
@@ -47,9 +55,10 @@ public class TutorialTrigger : MonoBehaviour
     
         if (other.gameObject.tag == "Player")
         {
+            countDown--;
             foreach (TutorialBubble tutorialBubble in tutorialBubbles)
             {
-                tutorialBubble.checktheBush(HintIndex);
+                tutorialBubble.checktheBush(hintIndex);
                 tutorialBubble.cleanText();
                 //Tween lastcall = DOVirtual.DelayedCall(3f, () => tutorialBubble.cleanText(), false);
             }
