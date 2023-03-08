f9fd4038c0bb88a7083bc7425c3c6b9d3441b197
DD-1111 <56623337+DD-1111@users.noreply.github.com>
2023-03-08 13:56:47 -0600

    patch fix on 2.3 and 2.4

--- Assets/- Scripts/Levels/trickPF.cs
+++ Assets/- Scripts/Levels/trickPF.cs
@@ -8,24 +8,59 @@ public class trickPF : MonoBehaviour
 
     private PlayerStateMachine _player;
 
-    [Tooltip("Button effect to trigger")]
-    [SerializeField] public ConfigurableButton button;
+    [Tooltip("Targets connected with")]
+    public List<GameObject> targets;
+
+    private bool _pressed;
+
 
     void Start()
     {
         _player = FindObjectsOfType<PlayerStateMachine>()[0];
-        _player.ChargeFlingEvent += button.DoTrigger;
+        _player.ChargeFlingEvent += DoTrigger;
 
     }
 
     void OnDestroy()
     {
-        _player.ChargeFlingEvent -= button.DoTrigger;
+        _player.ChargeFlingEvent -= DoTrigger;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // Enable to called from outside
+    public void DoTrigger()
+    {
+        if (!_pressed)
+        {
+            _pressed = true;
+            foreach (GameObject target in targets)
+            {
+                Functionality(target);
+            }
+        }
+      
+    }
+
+    // Since fingevent require a float
+    public void DoTrigger(float parameter)
+    {
+        DoTrigger();
+    }
+
+  
+    private void Functionality(GameObject target)
+    {
+        MonoBehaviour[] scripts = target.GetComponentsInChildren<MonoBehaviour>();
+
+        foreach (MonoBehaviour script in scripts)
+        {
+            Debug.Log("Change state " + script.name);
+            script.enabled = !script.enabled;
+        }
     }
 }
