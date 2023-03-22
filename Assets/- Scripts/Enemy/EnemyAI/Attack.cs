9b94a9be7e14da5bfcf62808697bf948e1773f70
Peter Hwang <petersh0317@utexas.edu>
2023-03-22 13:37:25 -0500

    fixed attack delay

--- Assets/- Scripts/Enemy/EnemyAI/Attack.cs
+++ Assets/- Scripts/Enemy/EnemyAI/Attack.cs
@@ -30,7 +30,7 @@ public class Attack : Node
         _attackCounter += Time.deltaTime;
         if (_attackCounter >= _attackTime) {
             //play attack animation
-            _enemyClaw.Claw(1.5f);
+            _enemyClaw.Claw(2.5f);
             _attackCounter = 0f;
         }
         state = NodeState.RUNNING;
