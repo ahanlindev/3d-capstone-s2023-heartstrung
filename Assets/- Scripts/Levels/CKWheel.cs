7c812086efeacb9fa806e74f9da64db63d6e9505
DD-1111 <beastblackga@outlook.com>
2023-03-26 16:24:42 -0500

    Rework CK2

---
+++ Assets/- Scripts/Levels/CKWheel.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CKWheel : MonoBehaviour
+{
+    public float rotationSpeed = 45f;   // The speed of the rotation in degrees per second
+
+    void Update()
+    {
+        // Calculate the amount of rotation for this frame
+        float rotationThisFrame = rotationSpeed * Time.deltaTime;
+
+        // Apply the rotation to the object's y-axis
+        transform.Rotate(Vector3.up, rotationThisFrame);
+    }
+}
