2fb16c9f80d68c83c7cae5b586410214849c3126
Zebo616 <stevio616@gmail.com>
2023-03-27 13:21:07 -0500

    started level select; fixed comic 5

--- Assets/- Scripts/GlobalEnums.cs
+++ Assets/- Scripts/GlobalEnums.cs
@@ -6,6 +6,7 @@ using System;
 public enum SceneID {
     INVALID = -1,
     MAINMENU,
+    LEVELSELECT,
     VICTORY,
     GAME_OVER,
     COMIC1,
@@ -31,6 +32,7 @@ public static class SceneIDsToString {
     public static string GetName(this SceneID name) => name switch {
         SceneID.INVALID => "Invalid", 
         SceneID.MAINMENU => "Main Menu",
+        SceneID.LEVELSELECT => "Level Select",
         SceneID.VICTORY => "Victory",
         SceneID.GAME_OVER => "Defeat",
         SceneID.COMIC1 => "Comic1",
