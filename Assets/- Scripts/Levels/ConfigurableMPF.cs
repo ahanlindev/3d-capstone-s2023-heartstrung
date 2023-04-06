ef5a588e95f54b7ae05c2d14295797366b6ab241
Alex Hanlin <gluethulhu@gmail.com>
2023-04-06 15:17:59 -0500

    add comments and region tags to ConfigurableMPF

--- Assets/- Scripts/Levels/ConfigurableMPF.cs
+++ Assets/- Scripts/Levels/ConfigurableMPF.cs
@@ -1,76 +1,127 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.Animations;
 using UnityEngine;
+using UnityEngine.Serialization;
 using DG.Tweening;
 
 public class ConfigurableMPF : MonoBehaviour
 {
 
-    [Tooltip("Add/delete empty objects to the target list to set new waypoints.")]
-    [SerializeField] public Transform[] targets;
+    #region Serialized Fields
+    
+    [FormerlySerializedAs("targets")]
+    [Tooltip("Add/delete empty objects to this target list to set new waypoints.")]
+    [SerializeField] public Transform[] _targets;
 
-    [Tooltip("Speed of movingpf, the duration is 1 / speed")]
+    [Tooltip("Speed of the moving platform, the duration from one waypoint to another is 1 / speed")]
     [SerializeField] public float _speed = 0.2f;
 
-    [Tooltip("Time for the platform waiting at one waypoint.")]
-    [SerializeField] public float hangTime = 3.0f;
+    [FormerlySerializedAs("hangTime")]
+    [Tooltip("Time that the platform waits at a waypoint.")]
+    [SerializeField] public float _hangTime = 3.0f;
 
-    [Tooltip("If it stop after reach the final wp of the list")]
-    [SerializeField] public bool oneWay;
+    [FormerlySerializedAs("oneWay")]
+    [Tooltip("If true, the platform stops after reaching the final waypoint of the list")]
+    [SerializeField] public bool _oneWay;
 
+    #endregion
+    
+    #region Private Fields
+    
+    /// <summary>
+    /// Index of current waypoint
+    /// </summary>
     private int _index = 0;
+    
+    /// <summary>
+    /// Timer that measures rest time and movement time between 2 waypoints
+    /// </summary>
     private float _localTimer = 0;
    
+    /// <summary>
+    /// Position of the platform at the end of the previous frame
+    /// </summary>
     private Vector3 _lastPosition;
+    
+    /// <summary>
+    /// Rigidbodies affected by the moving platform
+    /// </summary>
     private HashSet<Rigidbody> _collidedBodies;
     
-    private bool _moved;
-
-    private ParentConstraint pc;
-
+    /// <summary>
+    /// If true, platform will no longer move after reaching its current target waypoint
+    /// </summary>
+    private bool _oneWayFinished;
+    
+    #endregion
+    
+    #region MonoBehaviour Methods
+    
     // Update is called once per frame
-    void FixedUpdate()
+    private void FixedUpdate()
     {   
-        moveTo();
+        MoveTo();
         UpdateAttachedBodies();
     }
 
-    private void Awake() {
-        if (_collidedBodies == null) _collidedBodies = new HashSet<Rigidbody>();
-    }
+    private void Awake() { _collidedBodies ??= new HashSet<Rigidbody>(); }
     
     private void OnEnable() {
         _lastPosition = transform.position;
-        if (_collidedBodies == null) _collidedBodies = new HashSet<Rigidbody>();
-        transform.DOMove(targets[_index].position, 1 / _speed);
+        _collidedBodies ??= new HashSet<Rigidbody>();
+        transform.DOMove(_targets[_index].position, 1 / _speed);
     }
 
     private void OnDisable() {
         _collidedBodies = null;    
     }
-
-    void moveTo()
+    
+    private void OnCollisionEnter(Collision collision)
     {
+        _collidedBodies ??= new HashSet<Rigidbody>();
+        var temp = collision.collider.attachedRigidbody;
+        if (temp != null) _collidedBodies.Add(temp);
         
+        Transform playerTransform = collision.gameObject.transform;
+        playerTransform.SetParent(transform.parent);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        var temp = collision.collider.attachedRigidbody;
+        if (temp != null) _collidedBodies.Remove(temp);   
+        collision.gameObject.transform.SetParent(null);
+        collision.gameObject.transform.localScale = new Vector3(1, 1, 1);
+    }
+    
+    #endregion
+    
+    #region Private methods
+    
+    /// <summary>
+    /// Timing and logic for movement and rest. Called in FixedUpdate.
+    /// </summary>
+    private void MoveTo()
+    {
         if (_localTimer > 0)
         {
             _localTimer -= Time.fixedDeltaTime;
-        } else if (!_moved && transform.position == targets[_index].position)
+        } else if (!_oneWayFinished && transform.position == _targets[_index].position)
         {
-            _index = (_index + 1) % targets.Length;
-            transform.DOMove(targets[_index].position, 1 / _speed);
-            _localTimer = hangTime + 1 / _speed;
+            _index = (_index + 1) % _targets.Length;
+            transform.DOMove(_targets[_index].position, 1 / _speed);
+            _localTimer = _hangTime + 1 / _speed;
 
             // the final target wp, change the flag and stop
-            if (_index == targets.Length - 1 && oneWay)
+            if (_index == _targets.Length - 1 && _oneWay)
             {
-                _moved = true;
+                _oneWayFinished = true;
             }
-            
         }        
     }
-
+    
+    /// <summary>
+    /// Move all attached rigidbodies and update last position
+    /// </summary>
     private void UpdateAttachedBodies() 
     {
         if (_collidedBodies.Count != 0)
@@ -83,25 +134,6 @@ public class ConfigurableMPF : MonoBehaviour
         }
         _lastPosition = transform.position;
     }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        // Debug.Log(collision.gameObject.name + " enter");
-        if (_collidedBodies == null) _collidedBodies = new HashSet<Rigidbody>();
-        var temp = collision.collider.attachedRigidbody;
-        if (temp != null) _collidedBodies.Add(temp);
-        
-        Transform playerTransform = collision.gameObject.transform;
-        playerTransform.SetParent(transform.parent);
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        // Debug.Log(collision.gameObject.name + " exit");
-
-        var temp = collision.collider.attachedRigidbody;
-        if (temp != null) _collidedBodies.Remove(temp);   
-        collision.gameObject.transform.SetParent(null);
-        collision.gameObject.transform.localScale = new Vector3(1, 1, 1);
-    }
+    
+    #endregion
 }
